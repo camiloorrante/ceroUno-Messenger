5e0260ff90a3c8d7a34211ecf4ca1c4923707f87
Camilo Orrante Herrera <camilo.orrante@gmail.com>
2018-08-01 16:23:32 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CurrentChatComponent } from './components/current-chat/current-chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'mensajes/:user', component: CurrentChatComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
