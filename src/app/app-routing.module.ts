7010991fba438403d02db3236e827400ade19f05
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-08-01 16:14:29 -0500

    Se agregan las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CurrentChatComponent } from './components/current-chat/current-chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'mensajes/:user', component: CurrentChatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
